7fc7f6c09f9f7f6890e4f31ffe5b564d530ba84f
adrianlfns <adrianlfns@yahoo.com>
2025-11-24 09:25:11 -0500

    some more pubhilsh

--- publishToGitHubPagesDocFolder.ps1
+++ publishToGitHubPagesDocFolder.ps1
@@ -1,8 +1,2 @@
-# Cleanup docs folder
-Remove-Item -Path docs\* -Recurse -Force
-
 #publish
-dotnet publish -o docs
-
-#move the actuall site directly to the docs folder
-XCOPY docs\wwwroot docs /E /I
+dotnet publish -o publish
