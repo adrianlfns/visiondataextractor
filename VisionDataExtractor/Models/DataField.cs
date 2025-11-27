dc9ff5097862580c97856071da2e3d96376dcf68
adrianlfns <adrianlfns@yahoo.com>
2025-11-26 19:38:18 -0500

    removed some warnings

--- VisionDataExtractor/Models/DataField.cs
+++ VisionDataExtractor/Models/DataField.cs
@@ -5,8 +5,8 @@ namespace VisionDataExtractor.Models
     public class DataField
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Prompt { get; set; }
+        public string? Name { get; set; }
+        public string? Prompt { get; set; }
 
         public DataField()
         {
--- VisionDataExtractor/Models/ExtractionResult.cs
+++ VisionDataExtractor/Models/ExtractionResult.cs
@@ -6,7 +6,7 @@ namespace VisionDataExtractor.Models
 {
     public class ExtractionResult
     {
-        public string ImageName { get; set; }
+        public string? ImageName { get; set; }
         public Dictionary<string, string> ExtractedData { get; set; } = new Dictionary<string, string>();
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
         public List<DataField> DataFields { get; set; } = new List<DataField>();
